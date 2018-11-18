03ded2078dc018feda491e741e4fd19ea0148a8a
HasanAtir <37210475+HasanAtir@users.noreply.github.com>
2018-11-18 18:20:03 +0500

    Added randomly simulated war
    Testing is required

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <cmath>
 #include <ctime>
 #include <cstdlib>
+#include "functions.cpp"
 //Workdate/time 4:19, 18-Nov-2018
 
 using namespace std;
@@ -21,9 +22,11 @@ using namespace std;
  *
  *
  * */
-
-bool win_war_Skirmish(int& population, int& land, int& military);
+bool player_wins_war(int& player_pop, int& player_land, int& player_mili, int& neighbour_pop, int& neighbour_mili);
+bool win_war_Skirmish(int& population, int& land, int& military); //stub. Must be replaced!!!
 int d100_Random_Roll();
+int d10_Random_Roll();
+void player_Surrender(int& pop, int& land, int& military);
 
 void starvationFactor(int& population, int food_stores)
 {
@@ -68,10 +71,12 @@ int main()
     int north_commerce=10000000;
     int north_gdp;
     int north_military_factory=1;
+    int north_land=1000;
     //growth
     double north_population_growth = 1.05;
     double north_commerce_growth = 1.15;
     double north_military_growth = military_factory*25;
+
 
     int south_id=2;
     int east_id =3;
@@ -79,6 +84,8 @@ int main()
 
     while(!game_over)
     {
+        //resetting AI values
+        int north_land = 1000;
         //growth factors:
         gdp = (commerce)/population;
 
@@ -175,6 +182,51 @@ int main()
         north_military = north_military + north_military_growth;
         north_commerce=north_commerce*north_commerce_growth;
         gdp = north_commerce/north_population;
+        /*available options that should be available
+         * 1) war
+         * 2) build military factory
+         * 3) do nothing
+         * 4)
+         * */
+        int north_random_options = d10_Random_Roll();
+
+        if(north_random_options==1)
+        {   bool player_defends = false;
+            cout<<"\n"<<north_name<<" has declared war on you!\n";
+            cout<<"Press 1 to defend, or 0 to concede:\n";
+            cin>>player_defends;
+            if(player_defends)
+            {
+                bool player_victory= player_wins_war(population,land,military,north_population,north_military);
+                if(player_victory)
+                {
+                        cout<<"You have won the war!\n\n";
+                        number_of_wars_won++;
+                        // Happiness Effects
+                        if (happiness_factor < 100)
+                        {
+                            happiness_factor = happiness_factor +10;
+                        }
+                        else
+                        {
+                            happiness_factor;
+                        }
+                }
+                else
+                {
+                    cout<<"You have lost the war!\n\n";
+                    happiness_factor = happiness_factor -10;
+                }
+            }
+            else
+            {
+                cout<<"You surrendered\n";
+                player_Surrender(population, land, military);
+                happiness_factor = happiness_factor - 10;
+
+            }
+        }
+
 
 
         year++;
@@ -185,8 +237,17 @@ int main()
  * @Return = [0,100]*/
 int d100_Random_Roll()
 {
+    srand(time(0));
     int random = rand();
     int output = random%100;
+    return output;
+}
+
+int d10_Random_Roll()
+{
+    srand(time(0));
+    int random = rand();
+    int output = random%10;
     return output;
 }
 /**Function used to simulate skirmishes using chance.
@@ -215,4 +276,37 @@ bool win_war_Skirmish(int& population, int& land, int& military)
         military    =military   * loss_mil;
         return false;
     }
+}
+bool player_wins_war(int& player_pop, int& player_land, int& player_mili, int& neighbour_pop, int& neighbour_mili)
+{
+    int chance = d100_Random_Roll();
+    double win_pop = 0.90, win_land = 1.15, win_mil = 0.85;
+    double loss_pop= 0.75, loss_land= 0.90, loss_mil= 0.70;
+
+    if(chance>50)
+    {
+        player_pop      =player_pop         *win_pop;
+        player_land     =player_land        *win_land;
+        player_mili     =player_mili        *win_mil;
+        neighbour_pop   =neighbour_pop      *loss_pop;
+        neighbour_mili  =neighbour_mili     *loss_mil;
+        return true;
+    }
+    else
+    {
+        player_pop      =player_pop         *loss_pop;
+        player_land     =player_land        *loss_land;
+        player_mili     =player_mili        *loss_mil;
+        neighbour_pop   =neighbour_pop      *win_pop;
+        neighbour_mili  =neighbour_mili     *win_mil;
+        return false;
+    }
+}
+
+void player_Surrender(int& pop, int& land, int& military)
+{
+    double poploss = 0.95, landloss=0.80, milloss=0.80;
+    pop = pop*poploss;
+    land = land*landloss;
+    military = military*milloss;
 }
